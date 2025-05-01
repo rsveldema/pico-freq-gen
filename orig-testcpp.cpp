e83532851d527a34fb0f8b954d628532f7264cad
Ronald Veldema <rsveldema@gmail.com>
2025-05-01 12:42:18 +0200

    feat: Initial import

---
+++ orig-testcpp.cpp
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/dma.h"
+#include "hardware/pio.h"
+#include "hardware/interp.h"
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
+#include "pico/cyw43_arch.h"
+#include "hardware/uart.h"
+
+// Data will be copied from src to dst
+const char src[] = "Hello, world! (from DMA)";
+char dst[count_of(src)];
+
+#include "blink.pio.h"
+
+void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    blink_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+
+    printf("Blinking pin %d at %d Hz\n", pin, freq);
+
+    // PIO counter program takes 3 more cycles in total than we pass as
+    // input (wait for n + 1; mov; jmp)
+    pio->txf[sm] = (150000000 / (2 * freq)) - 3;
+}
+
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    // Put your timeout handler code in here
+    return 0;
+}
+
+
+
+// UART defines
+// By default the stdout UART is `uart0`, so we will use the second one
+#define UART_ID uart1
+#define BAUD_RATE 115200
+
+// Use pins 4 and 5 for UART1
+// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
+#define UART_TX_PIN 4
+#define UART_RX_PIN 5
+
+
+
+int main()
+{
+    stdio_init_all();
+
+    /*
+    if (! set_sys_clock_khz(170000, true))
+    {
+        printf("failed to set new clock speed\n");
+    }*/
+
+    // Initialise the Wi-Fi chip
+    if (cyw43_arch_init()) {
+        printf("Wi-Fi init failed\n");
+        return -1;
+    }
+
+    // Get a free channel, panic() if there are none
+    int chan = dma_claim_unused_channel(true);
+
+    // 8 bit transfers. Both read and write address increment after each
+    // transfer (each pointing to a location in src or dst respectively).
+    // No DREQ is selected, so the DMA transfers as fast as it can.
+
+    dma_channel_config c = dma_channel_get_default_config(chan);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
+    channel_config_set_read_increment(&c, true);
+    channel_config_set_write_increment(&c, true);
+
+    dma_channel_configure(
+        chan,          // Channel to be configured
+        &c,            // The configuration we just created
+        dst,           // The initial write address
+        src,           // The initial read address
+        count_of(src), // Number of transfers; in this case each is 1 byte.
+        true           // Start immediately.
+    );
+
+    // We could choose to go and do something else whilst the DMA is doing its
+    // thing. In this case the processor has nothing else to do, so we just
+    // wait for the DMA to finish.
+    dma_channel_wait_for_finish_blocking(chan);
+
+    // The DMA has now copied our text from the transmit buffer (src) to the
+    // receive buffer (dst), so we can print it out from there.
+    puts(dst);
+
+    // PIO Blinking example
+    PIO pio = pio0;
+    uint offset = pio_add_program(pio, &blink_program);
+    printf("Loaded program at %d\n", offset);
+
+    #ifdef PICO_DEFAULT_LED_PIN
+    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
+    #else
+    blink_pin_forever(pio, 0, offset, 6, 3);
+    #endif
+    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
+
+    // Interpolator example code
+    interp_config cfg = interp_default_config();
+    // Now use the various interpolator library functions for your use case
+    // e.g. interp_config_clamp(&cfg, true);
+    //      interp_config_shift(&cfg, 2);
+    // Then set the config
+    interp_set_config(interp0, 0, &cfg);
+    // For examples of interpolator use see https://github.com/raspberrypi/pico-examples/tree/master/interp
+
+    // Timer example code - This example fires off the callback after 2000ms
+    add_alarm_in_ms(2000, alarm_callback, NULL, false);
+    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+
+    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
+    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
+    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+
+    // Example to turn on the Pico W LED
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+
+    // Set up our UART
+    uart_init(UART_ID, BAUD_RATE);
+    // Set the TX and RX pins by using the function select on the GPIO
+    // Set datasheet for more information on function select
+    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
+    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+
+    // Use some the various UART functions to send out data
+    // In a default system, printf will also output via the default UART
+
+    // Send out a string, with CR/LF conversions
+    uart_puts(UART_ID, " Hello, UART!\n");
+
+    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
+
+    int x =0;
+    while (true) {
+        printf("Hello, world: %d\n", x++);
+        sleep_ms(1000);
+    }
+}
